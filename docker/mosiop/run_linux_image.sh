487a4cf1dec886e1c7874e6c6b089d9843858ef6
Marius Bagle <marius@NTNU24923>
2025-08-08 08:51:13 +0200

    latest

--- docker/mosiop/run_linux_image.sh
+++ docker/mosiop/run_linux_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo -e '\n \e[1;34mMounted project base directory:\033[0m\e[1;33m'$PWD'\033[0m\n'
-docker run -it --privileged --volume=$HOME"/.Xauthority:/root/.Xauthority:rw" --mount type=bind,source=$PWD,target=/home/$USER --env="DISPLAY" --net=host bacssaas/mosiop:latest
+docker run -it --privileged --volume=$HOME"/.Xauthority:/root/.Xauthority:rw" --mount type=bind,source=$PWD/../../code,target=/home/$USER --env="DISPLAY" --net=host bacssaas/mosiop:latest
 
